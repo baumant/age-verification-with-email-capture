ed10c458ba4e5850bc71714444990cc2b9e4e361
Timothy Bauman <timothy.bauman@wpromote.com>
2019-11-15 14:20:01 -0800

    it worked! remove old age-gate.js, add cookie double check to accomodate for async scripttag load

--- age-gate.js
+++
@@ -1,3 +0,0 @@
-console.log('--age gate script loaded');
-document.body.className += ' ' + 'noscroll';
-document.getElementById('prompt-background').style.display = 'flex';
--- public/age-gate.js
+++ public/age-gate.js
@@ -1,3 +1,12 @@
 console.log('--age gate script loaded');
+
+var twentyone = readCookie('twentyone');
+var emailcaptured = readCookie('emailcaptured');
+
 document.body.className += ' ' + 'noscroll';
 document.getElementById('prompt-background').style.display = 'flex';
+
+if (twentyone) {
+  // remove noscroll class if user is over 21, hide age gate
+  document.body.className = document.body.className.replace("noscroll","");
+};
