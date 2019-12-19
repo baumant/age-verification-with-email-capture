c485b66b5adb4aebcfd42fe7b87c7b8e2b16da27
Timothy Bauman <timothy.bauman@wpromote.com>
2019-12-19 10:39:15 -0800

    update script to remove old versions of app adding noscroll

--- public/age-verification-with-email-capture.js
+++ public/age-verification-with-email-capture.js
@@ -1,12 +1,6 @@
 console.log('--age verification with email capture script loaded');
 
-var twentyone = readCookie('twentyone');
-var emailcaptured = readCookie('emailcaptured');
+// remove old versions of app adding noscroll class
+document.body.className = document.body.className.replace("noscroll","");
 
-// document.body.className += ' ' + 'noscroll';
 document.getElementById('prompt-background').style.display = 'flex';
-
-if (twentyone) {
-  // remove noscroll class if user is over 21, hide age verification
-  document.body.className = document.body.className.replace("noscroll","");
-};
